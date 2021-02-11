aff573cc082b46bfd95f8d872de9d56052a5d0a9
mustafaekici <wustafaekici@hotmail.com>
2021-02-11 23:38:49 +0300

    Update CompositionRoot.groovy

--- CompositionRoot.groovy
+++ CompositionRoot.groovy
@@ -14,7 +14,7 @@ def Initialize(){
       //  Class parsedClass = groovyClassLoader.parseClass(groovyFile);
 
      // def t = new AnotherClass()
-     def m = new myclass()
+     def m = new BFolder.myclass()
       
      //return getClass().protectionDomain.codeSource.location.path
 }
